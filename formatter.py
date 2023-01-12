4c80acac3fd00480105f7c672af23b0838a016fc
ArturYastrebov <109103802+ArturYastrebov@users.noreply.github.com>
2023-01-12 15:40:43 +0200

    Add files via upload

---
+++ formatter.py
@@ -0,0 +1,77 @@
+import xlsxwriter
+import aiofiles
+from aiocsv import AsyncWriter
+from fpdf import FPDF
+
+
+async def save_csv(data, title):
+    async with aiofiles.open(f'{title}.csv', 'w', newline='', encoding="utf-8") as file:
+        writer = AsyncWriter(file)
+        await writer.writerow(
+            ['title',
+             'description',
+             'price',
+             'photo',
+             'main_photo']
+        )
+        await writer.writerows(data)
+    print(f'We used async def save_csv')
+    print(f'Data was saved to file {title}.csv')
+
+
+def save_pdf(data: list, title: str):
+    pdf = FPDF(format="A4")
+    pdf.add_font('DejaVu', '', 'font/DejaVuSansCondensed.ttf')
+    pdf.set_font("DejaVu", size=10)
+    count_4 = 0
+    n = 0
+    for data_block in data:
+        if count_4 == 4:
+            count_4 = 0
+            n = 0
+        count_4 += 1
+        if count_4 == 1:
+            pdf.add_page()
+        pdf.text(11, 15 + n, f"title : {data_block[0]}")
+        pdf.text(11, 20 + n, f"price : {data_block[2]}zl")
+        pdf.set_y(25 + n)
+        pdf.multi_cell(70, 5, f"descriptions : {data_block[1]}")
+        pdf.image(data_block[3], x=85, y=10 + n, h=50)
+        if len(data_block) == 5:
+            pdf.image(data_block[4], x=140, y=10 + n, h=50)
+        n += 70
+    print(f'We used def save_pdf')
+    print(f'Data was saved to file {title}.pdf')
+    pdf.output(f'{title}.pdf')
+
+
+def save_xlsx(data, title):
+    workbook = xlsxwriter.Workbook(f'{title}.xlsx')
+    worksheet = workbook.add_worksheet('Sheet1')
+    header = ['title',
+              'description',
+              'price',
+              'photo',
+              'main_photo']
+
+    for i, entry in enumerate(header):
+        worksheet.write(0, i, entry)
+    for index, entry in enumerate(data):
+        for i in range(5):
+            worksheet.write(index + 1, i, entry[i])
+    workbook.close()
+    print(f'We used def save_xlsx')
+    print(f'Data was saved to file {title}.xlsx')
+
+
+FORMATTER_DICT = {
+    'csv': save_csv,
+    'pdf': save_pdf,
+    'xlsx': save_xlsx,
+}
+
+
+def formatter_manu(FORMATTER_DICT):
+    formatter_menu_list = [(index, formatter) for index, formatter in
+                           enumerate(FORMATTER_DICT.keys(), start=1)]
+    return formatter_menu_list
---
+++ telegram_bot.py
@@ -0,0 +1,109 @@
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiofiles import os
+
+from async_parser.data_parser import format_str_command_manu, parser_items
+from async_parser.formatter import formatter_manu, FORMATTER_DICT, save_csv
+
+from config import TOKEN
+
+storage = MemoryStorage()
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot=bot,
+                storage=storage)
+
+COMMAND_MENU, COMMAND_DICT, IKEA_data = format_str_command_manu()
+formatter_menu_list = formatter_manu(FORMATTER_DICT)
+
+
+class ClientStatesGroup(StatesGroup):
+    category = State()
+    sub_category = State()
+    type_formatter = State()
+
+
+def get_cancel() -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+    kb.add(KeyboardButton('/cancel'))
+    return kb
+
+
+@dp.message_handler(commands='cancel', state='*')
+async def cmd_start(message: types.Message, state: FSMContext):
+    await state.finish()
+    await message.answer('Enter /start if you want to pars the IKEA shop')
+
+
+@dp.message_handler(commands='start')
+async def cmd_start(message: types.Message):
+    await bot.send_photo(chat_id=message.from_user.id,
+                         photo='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Ikea_logo.svg/'
+                               '1280px-Ikea_logo.svg.png')
+    await message.answer('Welcome to IKEA pars bot\nSelect the number of category you want to parse\n')
+    print(f'{message.from_user.id} on category menu')
+    await bot.send_message(chat_id=message.from_user.id, text=COMMAND_MENU)
+    await ClientStatesGroup.category.set()
+
+
+@dp.message_handler(lambda message: message.text in str(list(COMMAND_DICT.keys())) and message.text != "0",
+                    state=ClientStatesGroup.category)
+async def category_handler(message: types.Message, state: FSMContext):
+    if message.text[:1] != '/':
+        message.text = '/' + message.text
+    async with state.proxy() as data:
+        data['category'] = COMMAND_DICT[message.text]
+    await message.answer(f'You have selected the {COMMAND_DICT[message.text]} category')
+    await message.answer(f'Select a subcategory to parse')
+    answer_data = ''
+    for index, category in enumerate(IKEA_data[COMMAND_DICT[message.text]], start=1):
+        answer_data += f'/{index}' + ' - ' + category.get("title") + '\n'
+    print(f'{message.from_user.id} on sub_category menu')
+    await message.answer(answer_data, reply_markup=get_cancel())
+    await ClientStatesGroup.next()
+
+
+@dp.message_handler(lambda message: message.text,
+                    state=ClientStatesGroup.sub_category)
+async def sub_category_handler(message: types.Message, state: FSMContext):
+    if message.text[:1] != '/':
+        message.text = '/' + message.text
+    async with state.proxy() as data:
+        data['sub_category'] = IKEA_data[data['category']][int(message.text[1:]) - 1]
+        await message.answer(f'You have selected the {data["sub_category"]["title"]} subcategory')
+    await message.answer(f'Select the format for saving the result')
+    answer_data = ''
+    for formatter_data in formatter_menu_list:
+        answer_data += f'/{formatter_data[0]}' + ' - ' + formatter_data[1] + '\n'
+    print(f'{message.from_user.id} on type_formatter menu')
+    await message.answer(answer_data, reply_markup=get_cancel())
+    await ClientStatesGroup.next()
+
+
+@dp.message_handler(lambda message: message.text,
+                    state=ClientStatesGroup.type_formatter)
+async def type_formatter(message: types.Message, state: FSMContext):
+    if message.text[:1] != '/':
+        message.text = '/' + message.text
+    await message.answer(f'Start parsing...please wait')
+    async with state.proxy() as data:
+        data['type_formatter'] = formatter_menu_list[int(message.text[1:]) - 1][1]
+        print(f"{message.chat.id} try to pars in {data['type_formatter']}")
+        pars_data, title = parser_items(data['sub_category'])
+        if data['type_formatter'] in ['csv']:
+            await FORMATTER_DICT[data['type_formatter']](pars_data, title)
+        else:
+            FORMATTER_DICT[data['type_formatter']](pars_data, title)
+        await message.answer(f'Data was saved to file {title}.{data["type_formatter"]}')
+        await bot.send_document(chat_id=message.chat.id, document=open(f'{title}.{data["type_formatter"]}', 'r+b'))
+        print(f"{message.chat.id} got file in {title}.{data['type_formatter']}")
+        await os.remove(f'{title}.{data["type_formatter"]}')
+        await state.finish()
+        await message.answer('Enter /start if you want to pars the IKEA shop again')
+
+
+if __name__ == '__main__':
+    print('Bot has started work...')
+    executor.start_polling(dp)
