bc94381a0c9e475c9e901e8c7ae41573d129c5ed
AOYastrebov <AOYastrebov@gmail.com>
2023-04-18 19:12:10 +0300

    add task 2

--- training_pars_tasks/task2.py
+++ training_pars_tasks/task2.py
@@ -2,3 +2,10 @@
 #
 # Example binary string:
 # binary_str = "110101"
+
+def convert_str_binary_to_int(binary_str):
+    return sum([int(i)*count for count, i in enumerate(binary_str[::-1], start=1)])
+
+print(convert_str_binary_to_int("110101"))
+print(convert_str_binary_to_int("110"))
+print(convert_str_binary_to_int("101"))
